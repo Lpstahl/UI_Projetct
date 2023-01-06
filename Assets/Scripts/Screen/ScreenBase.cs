84fff23f09fe7c7a247b0d1061d4c81d75f7c089
Lpstahl <lpstahl.dev@gmail.com>
2023-01-06 17:52:00 -0300

    Update all (working)

---
+++ Assets/Scripts/Screen/ScreenBase.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using NaughtyAttributes;
+using DG.Tweening;
+
+namespace Screens
+{
+    public enum ScreenType
+    {
+        Panel,
+        Info_Panel,
+        Shop,
+        Info,
+        Inventory,
+        Messages,
+        Ranking,
+        Missions,
+        Settings,
+        Facebook
+    }
+
+    public class ScreenBase : MonoBehaviour
+    {
+        public ScreenType screenType;
+
+        public List<Transform> listObjects;
+        public List<Typper> listOfPhrases;
+
+        public Image uiBackground;
+
+        public bool startHided = false;
+
+        [Header("Animation")]
+        public float delayBetweenObjects = .05f;
+        public float animationDuration = .3f;
+
+        private void Start()
+        {
+            if (startHided)
+            {
+                HideObjects();
+            }
+        }
+
+        [Button]
+        public virtual void Show()
+        {
+            ShowObjects();
+            Debug.Log("Show");
+        }
+
+        [Button]
+        public virtual void Hide()
+        {
+            Debug.Log("Show");
+            HideObjects();
+        }
+
+        private void HideObjects()
+        {
+            listObjects.ForEach(i => i.gameObject.SetActive(false));
+            uiBackground.enabled = false;
+        }
+
+        private void ShowObjects()
+        {
+            for (var i = 0; i < listObjects.Count; i++)
+            {
+                var obj = listObjects[i];
+
+                obj.gameObject.SetActive(true);
+                obj.DOScale(0, animationDuration).From().SetDelay(i * delayBetweenObjects);
+            }
+
+            Invoke(nameof(StartType), delayBetweenObjects * listObjects.Count);
+            uiBackground.enabled = true;
+        }
+
+        private void StartType()
+        {
+            for (int i = 0; i < listOfPhrases.Count; i++)
+            {
+                listOfPhrases[i].StartType();
+            }
+        }
+
+        private void forceShowObjects()
+        {
+            listObjects.ForEach(i => i.gameObject.SetActive(true));
+            uiBackground.enabled = true;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Screen/ScreenManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ebac.Core.Singleton;
+
+namespace Screens
+{ 
+    public class ScreenManager : Singleton<ScreenManager>
+    {
+        public List<ScreenBase> screenBases;
+
+        public ScreenType startScreen = ScreenType.Panel;
+
+        private ScreenBase _currentScreen;
+
+        public Vector3 vec;
+
+        private void Start()
+        {
+            HideAll();
+            ShowByType(startScreen);
+        }
+
+        private void GetRandom()
+        {
+            screenBases[Random.Range(0, screenBases.Count)].animationDuration = 1;
+        }
+
+        public void ShowByType(ScreenType type)
+        {
+            if (_currentScreen != null) _currentScreen.Hide();
+
+            var nextScreen = screenBases.Find(i => i.screenType == type);
+            
+            nextScreen.Show();
+            _currentScreen = nextScreen;
+        }
+
+        public void HideAll()
+        {
+            screenBases.ForEach(i => i.Hide());
+        }
+    }
+
+}
